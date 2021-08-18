1a2dd9d4e087a13d935653e78f08a282eae1ac9f
Sanjana Joshi <66120868+sanjana-joshi14@users.noreply.github.com>
2021-08-18 11:57:40 +0530

    Update Sanjana_Stage1.java

--- Sanjana_Stage1.java
+++ Sanjana_Stage1.java
@@ -1,6 +1,4 @@
-#!usr/bin/java
-
-public class Sanjana_Stage1 {       
+public class java_Sanjana {       
     static int hammingDist(String str1, String str2)
 {
     int i = 0, count = 0;
@@ -15,14 +13,15 @@ public class Sanjana_Stage1 {
  
 public static void main (String[] args)
 {
-    System.out.println("Name: Sanjana Joshi");
-    System.out.println("Email id: sanjanajo868@gmailcom");    
-    System.out.println("Slack username: @Sanjana1404");     
-    System.out.println("Biostack: Genomics");  
+    System.out.println("Name:Sanjana Joshi");
+    System.out.println("Email:sanjanajo868@gmailcom");    
+    System.out.println("<Slack_username>:@Sanjana1404");  
+    System.out.println("<Twitter_handle>:@Sanjana1404"); 
+    System.out.println("Biostack:Genomics");     
+    
     String str1 = "Sanjana1404";
     String str2 = "Sanjana1404";
  
-    System.out.println("Hamming distance: "+ hammingDist (str1, str2));
+    System.out.println("<Hamming_Distance>:"+ hammingDist (str1, str2));
 }
 }    
-        
